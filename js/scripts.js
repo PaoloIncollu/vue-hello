0a8f8490aaa50b9595af34714e9add4962c1595e
PaoloIncollu <paoloinco@gmail.com>
2024-08-28 13:56:38 +0200

    caricamento esercizio completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 
-console.log('java collegato')
+const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        image: 'https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fll22z8mea106b4vjdjy7.jpeg'
+      }
+    }
+  }).mount('#app')
